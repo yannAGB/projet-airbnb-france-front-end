7361f691ab58303edc2cb0cc365083dc0d147568
AGBEHONOU Yann <yann-prince-djilan.agbehonou@etu.univ-lehavre.fr>
2026-06-09 17:00:23 +0200

    Mise a jour de la property-host

--- src/app/property-detail/property-host/property-host.ts
+++ src/app/property-detail/property-host/property-host.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PropertyHote } from '../../services/realEstate/real-estate-http-client-services';
 
 @Component({
   selector: 'app-property-host',
-  imports: [],
   templateUrl: './property-host.html',
   styleUrl: './property-host.css',
 })
-export class PropertyHostComponent {}
+export class PropertyHostComponent {
+  @Input() hote: PropertyHote | null = null;
+}
