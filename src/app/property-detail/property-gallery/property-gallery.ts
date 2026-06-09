de2d3c72ca035ba2363d9c87c1c7dfccdddce6d0
AGBEHONOU Yann <yann-prince-djilan.agbehonou@etu.univ-lehavre.fr>
2026-06-09 16:46:42 +0200

    Mise a jour de la property-gallery

--- src/app/property-detail/property-gallery/property-gallery.ts
+++ src/app/property-detail/property-gallery/property-gallery.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-property-gallery',
-  imports: [],
   templateUrl: './property-gallery.html',
   styleUrl: './property-gallery.css',
 })
-export class PropertyGalleryComponent {}
+export class PropertyGalleryComponent {
+  @Input() images: string[] = [];
+
+  imageActive = signal<number>(0);
+
+  get mainImage(): string {
+    return this.images[this.imageActive()] ?? 'https://picsum.photos/800/500';
+  }
+
+  get thumbnails(): string[] {
+    return this.images.slice(1, 5);
+  }
+
+  selectionner(index: number): void {
+    this.imageActive.set(index);
+  }
+}
